147f02f16fd41c89176e76d3954de8066e5d1731
Falkebr <falkebr@stud.ntnu.no>
2025-04-25 14:10:12 +0200

    done

--- frontend/src/pages/Homepage/Homepage.jsx
+++ frontend/src/pages/Homepage/Homepage.jsx
@@ -2,8 +2,19 @@ import styles from './Homepage.module.css';
 import SideMenu from '../../ui-components/SideMenu/SideMenu';
 import Feed from '../../ui-components/Feed/Feed';
 import SideFeed from '../../ui-components/SideFeed/SideFeed';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Homepage() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (!token) {
+            navigate('/login'); // Redirect to login page if no token is found
+        }
+    }, [navigate]);
+
     return (
         <div className={styles.home}>
             <SideMenu className={styles.home__sidemenu} />
