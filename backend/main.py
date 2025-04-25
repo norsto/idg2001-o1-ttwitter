ccd459bc2b452701dbc5001bee34f6ba843febd8
Memeius <marsbj@stud.ntnu.no>
2025-04-25 18:13:08 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,10 @@
 import uvicorn
-from .routes.account_routes import router as account_router
-from .routes.tweet_routes import router as tweet_router
+from backend.routes.account_routes import router as account_router
+from backend.routes.tweet_routes import router as tweet_router
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import os
-
 
 load_dotenv()
 
@@ -15,7 +14,7 @@ secret_key = os.getenv("SECRET_KEY")
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://temu-twitter.onrender.com"], # Erstatt "*" visst vi skal ha ett spesifikt domain (security measure for at andre applications ikkje kan bruke API'en)
+    allow_origins=["https://temu-twitter.onrender.com"],  # Replace "*" with your frontend's Render domain for better security
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -24,4 +23,4 @@ app.include_router(account_router)
 app.include_router(tweet_router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("backend.main:app", host="0.0.0.0", port=8000, reload=True)
